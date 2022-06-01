1cd15fba373e1aa95507d574227fcff5ad406d52
Peter <peter.hallstrom@gmail.com>
2022-06-02 00:21:23 +0200

    Fixed authentication

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -8,17 +8,26 @@ if (!OAUTH_CLIENT_ID) throw Error("OAUTH_CLIENT_ID not set");
 const oAuthClient = new OAuth2Client(OAUTH_CLIENT_ID);
 
 const validateIdToken = async (idToken: string) => {
-  const ticket = await oAuthClient.verifyIdToken({
-    idToken,
-    audience: OAUTH_CLIENT_ID,
-  });
-  return !!ticket.getPayload();
+  try {
+    await oAuthClient.verifyIdToken({
+      idToken,
+      audience: OAUTH_CLIENT_ID,
+    });
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 };
 
-const auth: RequestHandler = (req, res, next) => {
+const auth: RequestHandler = async (req, res, next) => {
+  const denyAccess = () => res.status(401).send("Unauthorized");
+
   const auth = getAuth(req);
-  if (!validateIdToken(auth)) return res.status(401).send("Unauthorized");
-  next();
+  if (!auth) return denyAccess();
+  const isTokenValid = await validateIdToken(auth);
+  if (!isTokenValid) denyAccess();
+  else next();
 };
 
 export default auth;
