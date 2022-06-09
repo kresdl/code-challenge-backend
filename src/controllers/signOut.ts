b70b152a12623c6a6dcb72972b08b83027f84771
Peter <peter.hallstrom@gmail.com>
2022-06-09 13:31:27 +0200

    Really added unsubscribe link

--- src/controllers/signOut.ts
+++ src/controllers/signOut.ts
@@ -1,9 +1,10 @@
 import { deleteUser } from "../models";
 import { AsyncRequestHandler } from "../types";
 
-const signOut: AsyncRequestHandler = async (_, res) => {
+const signOut: AsyncRequestHandler = async (req, res) => {
+  const userId = res.locals.userId ?? req.params.userId;
   try {
-    await deleteUser(res.locals.userId);
+    await deleteUser(userId);
     res.sendStatus(200);
   } catch (error) {
     res.status(500).send(error);
